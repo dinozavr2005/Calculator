72faeacf833bbc20d063a3982bdcf12493fd8776
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-10-31 21:13:10 +0300

    play

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,59 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Владимир on 31.10.2021.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    
+    //tuple
+    private var intermediateCalculation : (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+        
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return perfomTwoNumCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func perfomTwoNumCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+               return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not much any cases")
+            }
+        }
+        return nil
+    }
+}
